879c7f8ca17873beeb7a929b8c150205aa087d0e
jeff <jeffreyowens33@gmail.com>
2018-01-08 11:26:52 -0600

    reformatting for line legth

--- movies/media.py
+++ movies/media.py
@@ -1,12 +1,14 @@
 import webbrowser
 
+
 class Movie():
     """This class provides a way to store movie related information"""
     
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
     #this is to define the class Movie and what it initializes
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(
+        self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -15,5 +17,3 @@ class Movie():
     #this is to open the web browser and show the youtube trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
-
